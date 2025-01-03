740d5792b0ed207a28e93d95c51a1b2fddcfb082
Phelix <phelixmbani@gmail.com>
2025-01-04 00:33:06 +0300

    test-driven dev v2

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,10 @@
 // Your tests here
+import {pointsForWord} from '../utils'
+
+describe('pointsForWord', ()=>{
+    test('calculates the total points for a word(1 point for vowel, 2 per consonant', ()=>{
+        const word = 'word';
+        const points = pointsForWord(word);
+        expect(points).toBe(7);
+    });
+});
